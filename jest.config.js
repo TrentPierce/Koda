729f193899f41c907fe707bc1383944cd2c96e3e
Trent Pierce <Pierce.trent@gmail.com>
2026-02-04 23:41:29 -0600

    Update package.json with new dependencies and add test configuration with linting

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!src/**/index.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/__tests__/**/*.js'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/'
+  ],
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
+};
